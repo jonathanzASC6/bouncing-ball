48c6bebab2203863a07a03a0348825383defed5c
jonathanzASC6 <jzurita456@gmail.com>
2019-07-16 10:30:10 -0400

    hey

---
+++ sketch.js
@@ -0,0 +1,12 @@
+function setup(){
+    createCanvas(600,600);
+    background(0, 255, 0);
+}
+let posX = 0;
+function draw(){
+    background(0, 255, 0);
+ellipse(posX, 300, 60, 60);
+posX++
+}
+
+
